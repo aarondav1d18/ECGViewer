295733160bfda183d7789ed695d59ea90369fa4b
aarondav1d18 <aarond2005@icloud.com>
2025-12-10 15:23:03 +0000

    Updated docker scirpts

--- docker/build_and_run.bash
+++ docker/build_and_run.bash
@@ -1,8 +1,23 @@
-sudo docker build -t ecg-plotter .
+#!/usr/bin/env bash
+set -euo pipefail
 
-# Run
+IMAGE_NAME="ecg-plotter"
+
+cp Dockerfile ../
+
+# Enable local X access temporarily
+xhost +SI:localuser:root >/dev/null
+
+# Build the image
+sudo docker build -t "$IMAGE_NAME" ../
+
+# Run container with X11 support
 sudo docker run -it --rm \
-  -e DISPLAY=$DISPLAY \
-  -v /tmp/.X11-unix:/tmp/.X11-unix \
-  ecg-plotter
+  -e DISPLAY="${DISPLAY:?DISPLAY not set}" \
+  -v /tmp/.X11-unix:/tmp/.X11-unix:ro \
+  "$IMAGE_NAME"
+
+# Revert permissions
+xhost -SI:localuser:root >/dev/null
+rm ../Dockerfile
 
